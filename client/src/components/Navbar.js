2a06f10ff27ea1041f8bc0ba46e4acd93b36e5fb
Juan De Jesus <jdejesus93@gmail.com>
2020-02-05 22:43:09 -0400

    WIP

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,20 +2,14 @@ import React from "react";
 
 function Navbar(props)  {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <span className="navbar-brand">{props.title}</span>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    {props.children}
-                </ul>
-                <div className="my-2 my-lg-0">
-                    <button className="btn btn-outline-light my-2 my-sm-0" type="button" onClick={props.handleLogout}>Logout</button>
-                </div>
+                {props.children}
             </div>
         </nav>
     );
