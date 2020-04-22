e6c15c12afe1cf306a269b5a0e3ea52671842810
Juan De Jesus <jdejesus93@gmail.com>
2020-04-21 20:44:10 -0400

    Minor fixes

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = require("../../connection");
+const sequelize = require("../connection");
 const Model = Sequelize.Model;
 
 class Post extends Model {}
