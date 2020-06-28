5439f03172780f286130368a3b635dd3d0de6415
HectorRS3 <hector.emanuelramos@gmail.com>
2020-06-27 20:32:07 -0400

    Resolved conflicts from merge

---
+++ src/components/UsersComponents/CreateActividadesModal.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {Modal, Button} from 'react-bootstrap';
+function CreateActividadesModal() {
+    const [show, setShow] = useState(false);
+    
+    const handleShow = () => setShow(true);
+    const handleHide = () => setShow(false);
+
+    return(
+        <>
+          <Button variant="outline-light" onClick={handleShow}>Create Activity</Button>
+          <Modal show={show} onHide={handleHide}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create Activity</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <UserType edit={false}/>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
+
+export default CreateActividadesModal;
