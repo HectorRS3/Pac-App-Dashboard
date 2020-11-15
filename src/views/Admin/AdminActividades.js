93a1b480ba29c28d64156e2e547bd8dbaa451bc1
Juan De Jesus <jdejesus93@gmail.com>
2020-11-14 22:31:39 -0400

    Fixing component import directories

--- src/views/Admin/AdminActividades.js
+++ src/views/Admin/AdminActividades.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import Axios from 'axios'
 import { Navbar, Nav, Table, Container } from 'react-bootstrap'
 import CreateActividadesModal from '../../components/EventComponents/CreateActividadesModal'
+import EditActividadesModal from '../../components/EventComponents/EditActividadesModal'
+import DeleteActividadesModal from '../../components/EventComponents/DeleteActividadesModal'
 
 function AdminActividades() {
     const [state, setState] = useState(undefined)
