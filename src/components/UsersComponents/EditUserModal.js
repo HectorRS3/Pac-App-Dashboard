37e11f8bc1f39b0bb325569caa8b744983744591
Juan De Jesus <jdejesus93@gmail.com>
2021-05-18 19:39:28 -0400

    Edit User Modal changes

--- src/components/UsersComponents/EditUserModal.js
+++ src/components/UsersComponents/EditUserModal.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
-import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Form, Modal, Button } from 'react-bootstrap';
-import useInput from '../../hooks/useInput'
 import { UsersAPI } from '../../api';
 
 function EditUserModal(props) {
     const [show, setShow] = useState(false);
 
-    const [firstName, setFirstName] = useInput("");
-    const [lastName, setLastName] = useInput("");
-    const [username, setUsername] = useInput("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        getUserData(props.userId)
+        .then(data => {
+            setFirstName(data.firstName);
+            setLastName(data.lastName);
+            setUsername(data.username);
+        })
+        .catch(error => console.error(error));
+
+        return function cleanup() {
+            console.log("DONE!")
+        }
+    }, []);
 
     const handleShow = () => setShow(true);
     const handleHide = () => setShow(false);
+
+    const getUserData = async id => {
+        const userData = await UsersAPI().getUserById(id);
+        return userData;
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -45,17 +62,17 @@ function EditUserModal(props) {
 
                         <Form.Group controlId="formBasicFirstName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" name="firstName" placeholder="Enter First Name" value={firstName} onChange={setFirstName} />
+                            <Form.Control type="text" name="firstName" placeholder="Enter First Name" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicLastName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" name="lastName" placeholder="Enter Last Name" value={lastName} onChange={setLastName} />
+                            <Form.Control type="text" name="lastName" placeholder="Enter Last Name" value={lastName} onChange={(event) => setLastName(event.target.value)} />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicUsername">
                             <Form.Label>Username </Form.Label>
-                            <Form.Control type="text" name="username" placeholder="Enter Username" value={username} onChange={setUsername} />
+                            <Form.Control type="text" name="username" placeholder="Enter Username" value={username} onChange={(event) => setUsername(event.target.value)} />
                         </Form.Group>
 
                     </Form>
